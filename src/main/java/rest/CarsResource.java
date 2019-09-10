e8f075a8c44697ea2a484877dc3e81e419e8f148
Christian <rygerlunger@gmail.com>
2019-09-10 09:04:05 +0200

    Added Cars entity, Cars facade, cars resource, cars facadeTest, cars resourcetest.

---
+++ src/main/java/rest/CarsResource.java
@@ -0,0 +1,57 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entities.Joke;
+import utils.EMF_Creator;
+import facades.JokeFacade;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//Todo Remove or change relevant parts before ACTUAL use
+@Path("xxx")
+public class CarsResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
+                "pu",
+                "jdbc:mysql://localhost:3307/CA1DB",
+                "dev",
+                "ax2",
+                EMF_Creator.Strategy.CREATE);
+    private static final JokeFacade FACADE =  JokeFacade.getJokeFacade(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+            
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String demo() {
+        return "{\"msg\":\"Hello World\"}";
+    }
+    @Path("count")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getRenameMeCount() {
+        long count = FACADE.getRenameMeCount();
+        //System.out.println("--------------->"+count);
+        return "{\"count\":"+count+"}";  //Done manually so no need for a DTO
+    }
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void create(Joke entity) {
+        throw new UnsupportedOperationException();
+    }
+    
+    @PUT
+    @Path("/{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void update(Joke entity, @PathParam("id") int id) {
+        throw new UnsupportedOperationException();
+    }
+}
