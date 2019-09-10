cfa4e31a9e4e5ecd2919f0b1c4d4a2cab89c7abb
Christian <rygerlunger@gmail.com>
2019-09-10 09:36:57 +0200

    updated carsFacade

--- src/main/java/facades/CarsFacade.java
+++ src/main/java/facades/CarsFacade.java
@@ -5,10 +5,41 @@
  */
 package facades;
 
+import entities.Cars;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+
 /**
  *
  * @author Ryge <your.name at your.org>
  */
 public class CarsFacade {
+
+    private static CarsFacade instance;
+    private static EntityManagerFactory emf;
+
+    public CarsFacade() {
+    }
+
+    public static CarsFacade getCarsFacade(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new CarsFacade();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
     
+        public List<Cars> getCars(){
+        EntityManager em = getEntityManager();
+        TypedQuery tq = em.createNamedQuery("cars.all", Cars.class);
+        List<Cars> cars = tq.getResultList();
+        return cars;
+    }
+
 }
