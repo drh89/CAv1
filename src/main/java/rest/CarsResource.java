01d9e4c45fd4d75b49afaabdba5aa23761ab3fd1
Christian <rygerlunger@gmail.com>
2019-09-10 09:17:14 +0200

    Changed rest endpoint for carsResource from xxx - to cars

--- src/main/java/rest/CarsResource.java
+++ src/main/java/rest/CarsResource.java
@@ -16,7 +16,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 //Todo Remove or change relevant parts before ACTUAL use
-@Path("xxx")
+@Path("cars")
 public class CarsResource {
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
